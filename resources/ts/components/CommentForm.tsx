b692f17fe721560dedf7c09643bf6597692eccaf
Shiori_Yabe <sh_0418_ty@yahoo.co.jp>
2024-05-13 08:57:11 +0900

    Change layout of CommentForm component

--- resources/ts/components/CommentForm.tsx
+++ resources/ts/components/CommentForm.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import PrimaryButton from "./PrimaryButton";
 
 export const CommentForm = () => {
-    return <div>CommentForm</div>;
+    return <div>
+        <div className="max-h-40 overflow-y-auto w-11/12"
+        >
+            {/* 保存されたコメントを表示 */}
+            <div className="border border-white p-2 mb-2 break-words">
+                <p className="text-2xl font-bold m-0">コメントしたユーザー名
+                </p>
+                <p>ここにコメント内容が入ります</p>
+            </div>
+        </div>
+        {/* コメント入力フォーム */}
+        <div className="m-4">
+            <form >
+                <textarea className="bg-gray-900 text-white w-11/12 rounded-lg border-2 border-white"
+                />
+                <div className="text-right">
+                    <PrimaryButton type="submit">コメント</PrimaryButton>
+                </div>
+            </form>
+        </div>
+    </div>
 };
